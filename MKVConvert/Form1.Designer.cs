5f82c3a49eb88dff6911bac2c2391c6014e7464a
john.lewis <john.lewis@P03572>
2016-04-19 10:27:47 +0100

    Add project files.

---
+++ MKVConvert/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace MKVConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSD = new System.Windows.Forms.Button();
+            this.txtBxSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpBxFiles = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBxHD2HDBR = new System.Windows.Forms.TextBox();
+            this.txtBxHD2HDHei = new System.Windows.Forms.TextBox();
+            this.txtBxHD2HDWid = new System.Windows.Forms.TextBox();
+            this.txtBxHDBR = new System.Windows.Forms.TextBox();
+            this.txtBxHDHei = new System.Windows.Forms.TextBox();
+            this.txtBxHDWid = new System.Windows.Forms.TextBox();
+            this.txtBxSDBR = new System.Windows.Forms.TextBox();
+            this.txtBxSDHei = new System.Windows.Forms.TextBox();
+            this.txtBxSDWid = new System.Windows.Forms.TextBox();
+            this.btnHD = new System.Windows.Forms.Button();
+            this.btnHD2HD = new System.Windows.Forms.Button();
+            this.btnSourceBrow = new System.Windows.Forms.Button();
+            this.grpBxFiles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSD
+            // 
+            this.btnSD.Location = new System.Drawing.Point(49, 86);
+            this.btnSD.Name = "btnSD";
+            this.btnSD.Size = new System.Drawing.Size(63, 73);
+            this.btnSD.TabIndex = 0;
+            this.btnSD.Text = "SD";
+            this.btnSD.UseVisualStyleBackColor = true;
+            this.btnSD.Click += new System.EventHandler(this.btnSD_Click);
+            // 
+            // txtBxSource
+            // 
+            this.txtBxSource.Location = new System.Drawing.Point(59, 19);
+            this.txtBxSource.Name = "txtBxSource";
+            this.txtBxSource.Size = new System.Drawing.Size(188, 20);
+            this.txtBxSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source";
+            // 
+            // grpBxFiles
+            // 
+            this.grpBxFiles.Controls.Add(this.label8);
+            this.grpBxFiles.Controls.Add(this.label9);
+            this.grpBxFiles.Controls.Add(this.label10);
+            this.grpBxFiles.Controls.Add(this.label5);
+            this.grpBxFiles.Controls.Add(this.label6);
+            this.grpBxFiles.Controls.Add(this.label7);
+            this.grpBxFiles.Controls.Add(this.label4);
+            this.grpBxFiles.Controls.Add(this.label3);
+            this.grpBxFiles.Controls.Add(this.label2);
+            this.grpBxFiles.Controls.Add(this.txtBxHD2HDBR);
+            this.grpBxFiles.Controls.Add(this.txtBxHD2HDHei);
+            this.grpBxFiles.Controls.Add(this.txtBxHD2HDWid);
+            this.grpBxFiles.Controls.Add(this.txtBxHDBR);
+            this.grpBxFiles.Controls.Add(this.txtBxHDHei);
+            this.grpBxFiles.Controls.Add(this.txtBxHDWid);
+            this.grpBxFiles.Controls.Add(this.txtBxSDBR);
+            this.grpBxFiles.Controls.Add(this.txtBxSDHei);
+            this.grpBxFiles.Controls.Add(this.txtBxSDWid);
+            this.grpBxFiles.Controls.Add(this.btnHD);
+            this.grpBxFiles.Controls.Add(this.btnHD2HD);
+            this.grpBxFiles.Controls.Add(this.btnSD);
+            this.grpBxFiles.Controls.Add(this.btnSourceBrow);
+            this.grpBxFiles.Controls.Add(this.label1);
+            this.grpBxFiles.Controls.Add(this.txtBxSource);
+            this.grpBxFiles.Location = new System.Drawing.Point(13, 13);
+            this.grpBxFiles.Name = "grpBxFiles";
+            this.grpBxFiles.Size = new System.Drawing.Size(295, 339);
+            this.grpBxFiles.TabIndex = 6;
+            this.grpBxFiles.TabStop = false;
+            this.grpBxFiles.Text = "Files";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(126, 302);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "BitRate";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(126, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Height";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(126, 249);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Width";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(126, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "BitRate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(126, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Height";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(126, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "BitRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Width";
+            // 
+            // txtBxHD2HDBR
+            // 
+            this.txtBxHD2HDBR.Location = new System.Drawing.Point(183, 300);
+            this.txtBxHD2HDBR.Name = "txtBxHD2HDBR";
+            this.txtBxHD2HDBR.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHD2HDBR.TabIndex = 17;
+            this.txtBxHD2HDBR.Text = "512";
+            // 
+            // txtBxHD2HDHei
+            // 
+            this.txtBxHD2HDHei.Location = new System.Drawing.Point(183, 273);
+            this.txtBxHD2HDHei.Name = "txtBxHD2HDHei";
+            this.txtBxHD2HDHei.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHD2HDHei.TabIndex = 16;
+            this.txtBxHD2HDHei.Text = "720";
+            // 
+            // txtBxHD2HDWid
+            // 
+            this.txtBxHD2HDWid.Location = new System.Drawing.Point(183, 246);
+            this.txtBxHD2HDWid.Name = "txtBxHD2HDWid";
+            this.txtBxHD2HDWid.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHD2HDWid.TabIndex = 15;
+            this.txtBxHD2HDWid.Text = "1280";
+            // 
+            // txtBxHDBR
+            // 
+            this.txtBxHDBR.Location = new System.Drawing.Point(183, 220);
+            this.txtBxHDBR.Name = "txtBxHDBR";
+            this.txtBxHDBR.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHDBR.TabIndex = 14;
+            this.txtBxHDBR.Text = "512";
+            // 
+            // txtBxHDHei
+            // 
+            this.txtBxHDHei.Location = new System.Drawing.Point(183, 193);
+            this.txtBxHDHei.Name = "txtBxHDHei";
+            this.txtBxHDHei.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHDHei.TabIndex = 13;
+            // 
+            // txtBxHDWid
+            // 
+            this.txtBxHDWid.Location = new System.Drawing.Point(183, 166);
+            this.txtBxHDWid.Name = "txtBxHDWid";
+            this.txtBxHDWid.Size = new System.Drawing.Size(69, 20);
+            this.txtBxHDWid.TabIndex = 12;
+            // 
+            // txtBxSDBR
+            // 
+            this.txtBxSDBR.Location = new System.Drawing.Point(183, 139);
+            this.txtBxSDBR.Name = "txtBxSDBR";
+            this.txtBxSDBR.Size = new System.Drawing.Size(69, 20);
+            this.txtBxSDBR.TabIndex = 11;
+            this.txtBxSDBR.Text = "512";
+            // 
+            // txtBxSDHei
+            // 
+            this.txtBxSDHei.Location = new System.Drawing.Point(183, 112);
+            this.txtBxSDHei.Name = "txtBxSDHei";
+            this.txtBxSDHei.Size = new System.Drawing.Size(69, 20);
+            this.txtBxSDHei.TabIndex = 10;
+            this.txtBxSDHei.Text = "404";
+            // 
+            // txtBxSDWid
+            // 
+            this.txtBxSDWid.Location = new System.Drawing.Point(183, 86);
+            this.txtBxSDWid.Name = "txtBxSDWid";
+            this.txtBxSDWid.Size = new System.Drawing.Size(69, 20);
+            this.txtBxSDWid.TabIndex = 9;
+            this.txtBxSDWid.Text = "720";
+            // 
+            // btnHD
+            // 
+            this.btnHD.Location = new System.Drawing.Point(49, 166);
+            this.btnHD.Name = "btnHD";
+            this.btnHD.Size = new System.Drawing.Size(63, 74);
+            this.btnHD.TabIndex = 8;
+            this.btnHD.Text = "HD";
+            this.btnHD.UseVisualStyleBackColor = true;
+            this.btnHD.Click += new System.EventHandler(this.btnHD_Click);
+            // 
+            // btnHD2HD
+            // 
+            this.btnHD2HD.Location = new System.Drawing.Point(49, 246);
+            this.btnHD2HD.Name = "btnHD2HD";
+            this.btnHD2HD.Size = new System.Drawing.Size(63, 74);
+            this.btnHD2HD.TabIndex = 7;
+            this.btnHD2HD.Text = "HD2HD";
+            this.btnHD2HD.UseVisualStyleBackColor = true;
+            this.btnHD2HD.Click += new System.EventHandler(this.btnHD2HD_Click);
+            // 
+            // btnSourceBrow
+            // 
+            this.btnSourceBrow.Location = new System.Drawing.Point(83, 45);
+            this.btnSourceBrow.Name = "btnSourceBrow";
+            this.btnSourceBrow.Size = new System.Drawing.Size(133, 23);
+            this.btnSourceBrow.TabIndex = 6;
+            this.btnSourceBrow.Text = "Browse";
+            this.btnSourceBrow.UseVisualStyleBackColor = true;
+            this.btnSourceBrow.Click += new System.EventHandler(this.btnSourceBrow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 362);
+            this.Controls.Add(this.grpBxFiles);
+            this.Name = "Form1";
+            this.Text = "Convert";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpBxFiles.ResumeLayout(false);
+            this.grpBxFiles.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSD;
+        private System.Windows.Forms.TextBox txtBxSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpBxFiles;
+        private System.Windows.Forms.Button btnSourceBrow;
+        private System.Windows.Forms.Button btnHD;
+        private System.Windows.Forms.Button btnHD2HD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBxHD2HDBR;
+        private System.Windows.Forms.TextBox txtBxHD2HDHei;
+        private System.Windows.Forms.TextBox txtBxHD2HDWid;
+        private System.Windows.Forms.TextBox txtBxHDBR;
+        private System.Windows.Forms.TextBox txtBxHDHei;
+        private System.Windows.Forms.TextBox txtBxHDWid;
+        private System.Windows.Forms.TextBox txtBxSDBR;
+        private System.Windows.Forms.TextBox txtBxSDHei;
+        private System.Windows.Forms.TextBox txtBxSDWid;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MKVConvert/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MKVConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSD_Click(object sender, EventArgs e)
+        {
+            EncoderSD();   
+        }
+
+
+        //private void EncoderSD(int width, int height, int bitrate, bool copyAudio = true)
+        private void EncoderSD(bool copyAudio = true)
+        {
+            string[] files = Directory.GetFiles(txtBxSource.Text, "*.*");
+
+            foreach (string currentFile in files)
+            {
+                if (currentFile.EndsWith("avi") || currentFile.EndsWith("mp4") || currentFile.EndsWith("mkv") || currentFile.EndsWith("ts"))
+                {
+                    string sourcePath = @"F:\Convert";
+                    string directoryPath = Path.GetDirectoryName(txtBxSource.Text);
+
+                    foreach (var srcPath in Directory.GetFiles(sourcePath))
+                    {
+                        MessageBox.Show(srcPath + ", " + srcPath.Replace(sourcePath, directoryPath));
+                        //File.Copy(srcPath, srcPath.Replace(sourcePath, directoryPath), true);
+                    }
+
+
+                    MessageBox.Show("END");
+
+
+                    // Create launch settings
+
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.FileName = @"C:\Program Files\Handbrake\HandBrakeCLI.exe";
+                    if (copyAudio)
+                    {
+                        psi.Arguments = "-i \"" + currentFile + "\" -t 1 -o \"" + currentFile + "-output.mkv\" -f mkv -O -w " + txtBxSDWid.Text + " -l " + txtBxSDHei.Text + " --modulus 8 -e x264 -b " + txtBxSDBR.Text + " -2 --vfr -a 1 -E faac -B 160 -6 dpl2 -R Auto -D 0 --gain=0 --audio-copy-mask none --audio-fallback ffac3 -x weightp=1:subq=10:rc-lookahead=10:trellis=2:b-adapt=2:psy-rd=1.00,0.10 --verbose=1";
+                    }
+                    else
+                    {
+                        //psi.Arguments = "-i \"" + currentFile + "\" -t 1 -o \"" + currentFile + "-output.mkv\" -f mkv -O -w " + width + " -l " + height + " --modulus 8 -e x264 -b " + bitrate + " -2 --vfr -a 1 -E faac -B 160 -6 dpl2 -R Auto -D 0 --gain=0 --audio-copy-mask none --audio-fallback ffac3 -x weightp=1:subq=10:rc-lookahead=10:trellis=2:b-adapt=2:psy-rd=1.00,0.10 --verbose=1";
+                    }
+
+                    // Create process based on settings
+                    Process prc = new Process();
+                    prc.StartInfo = psi;
+                    prc.Start();
+
+                    // Wait for process to exit
+                    while (!prc.HasExited)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    // Sort file out
+                    if (File.Exists(currentFile + "-output.mkv"))
+                    {
+                        if (Directory.Exists(@"F:\Convert"))
+                        {
+                            File.Move(currentFile + "-output.mkv", @"F:\Convert\" + Path.GetFileName(currentFile));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Convert folder is missing or inaccessible");
+                        }
+
+                        string[] filePaths = Directory.GetFiles(txtBxSource.Text);
+                        foreach (string filePath in filePaths)
+                        {
+                            File.Delete(filePath);
+                        }
+                    }
+
+                    //string sourcePath = @"F:\Convert\";
+                    //string directoryPath = Path.GetDirectoryName(txtBxSource.Text);
+
+                    ////if (!Directory.Exists(directoryPath))
+                    ////{
+                    ////    Directory.CreateDirectory(directoryPath);
+                    ////}
+
+                    //foreach (var srcPath in Directory.GetFiles(sourcePath))
+                    //{
+                    //    MessageBox.Show(srcPath + ", " + srcPath.Replace(sourcePath, directoryPath));
+                    //    //File.Copy(srcPath, srcPath.Replace(sourcePath, directoryPath), true);
+                    //}
+                }
+            }
+        }
+
+        private void btnHD_Click(object sender, EventArgs e)
+        {
+            EncoderHD();
+        }
+
+        private void EncoderHD(bool copyAudio = true)
+        {
+            string[] files = Directory.GetFiles(txtBxSource.Text, "*.*");
+
+            foreach (string currentFile in files)
+            {
+                if (currentFile.EndsWith("avi") || currentFile.EndsWith("mp4") || currentFile.EndsWith("mkv"))
+                {
+                    // Create launch settings
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.FileName = @"C:\Program Files\Handbrake\HandBrakeCLI.exe";
+                    if (copyAudio)
+                    {
+                        psi.Arguments = "-i \"" + currentFile + "\" -t 1 -o \"" + currentFile + "-output.mkv\" -f mkv -O -e x264 -b " + txtBxHDBR.Text + " -2 --vfr -a 1 -E faac -B 160 -6 dpl2 -R Auto -D 0 --gain=0 --audio-copy-mask none --audio-fallback ffac3 -x weightp=1:subq=10:rc-lookahead=10:trellis=2:b-adapt=2:psy-rd=1.00,0.10 --verbose=1";
+                    }
+                    else
+                    {
+                        
+                    }
+
+                    // Create process based on settings
+                    Process prc = new Process();
+                    prc.StartInfo = psi;
+                    prc.Start();
+
+                    // Wait for process to exit
+                    while (!prc.HasExited)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    // Sort file out
+                    if (File.Exists(currentFile + "-output.mkv"))
+                    {
+                        if (Directory.Exists(@"F:\Convert"))
+                        {
+                            File.Move(currentFile + "-output.mkv", @"F:\Convert\" + Path.GetFileName(currentFile));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Convert folder is missing or inaccessible");
+                        }
+
+                        string[] filePaths = Directory.GetFiles(txtBxSource.Text);
+                        foreach (string filePath in filePaths)
+                        {
+                            File.Delete(filePath);
+                        }
+                    }
+                    //string sourcePath = @"F:\Convert";
+                    //string targetPath = txtBxSource.Text;
+                    //if (!Directory.Exists(targetPath))
+                    //{
+                    //    Directory.CreateDirectory(targetPath);
+                    //}
+                    //foreach (var srcPath in Directory.GetFiles(sourcePath))
+                    //{
+                    //    File.Copy(srcPath, srcPath.Replace(sourcePath, targetPath), true);
+                    //}
+                }
+            }
+        }
+        private void btnHD2HD_Click(object sender, EventArgs e)
+        {
+            EncoderHD2HD();
+        }
+
+        private void EncoderHD2HD(bool copyAudio = true)
+        {
+            string[] files = Directory.GetFiles(txtBxSource.Text, "*.*");
+
+            foreach (string currentFile in files)
+            {
+                if (currentFile.EndsWith("avi") || currentFile.EndsWith("mp4") || currentFile.EndsWith("mkv"))
+                {
+                    // Create launch settings
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.FileName = @"C:\Program Files\Handbrake\HandBrakeCLI.exe";
+                    if (copyAudio)
+                    {
+                        psi.Arguments = "-i \"" + currentFile + "\" -t 1 -o \"" + currentFile + "-output.mkv\" -f mkv -O -w " + txtBxHD2HDWid + " -l " + txtBxHD2HDHei + " --modulus 8 -e x264 -b " + txtBxHD2HDBR + " -2 --vfr -a 1 -E faac -B 160 -6 dpl2 -R Auto -D 0 --gain=0 --audio-copy-mask none --audio-fallback ffac3 -x weightp=1:subq=10:rc-lookahead=10:trellis=2:b-adapt=2:psy-rd=1.00,0.10 --verbose=1";
+                    }
+                    else
+                    {
+
+                    }
+
+                    // Create process based on settings
+                    Process prc = new Process();
+                    prc.StartInfo = psi;
+                    prc.Start();
+
+                    // Wait for process to exit
+                    while (!prc.HasExited)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    // Sort file out
+                    if (File.Exists(currentFile + "-output.mkv"))
+                    {
+                        if (Directory.Exists(@"F:\Convert"))
+                        {
+                            File.Move(currentFile + "-output.mkv", @"F:\Convert\" + Path.GetFileName(currentFile));
+                        }
+                        else
+                        {
+                            MessageBox.Show("Convert folder is missing or inaccessible");
+                        }
+
+                        string[] filePaths = Directory.GetFiles(txtBxSource.Text);
+                        foreach (string filePath in filePaths)
+                        {
+                            File.Delete(filePath);
+                        }
+                    }
+                    //string sourcePath = @"F:\Convert";
+                    //string targetPath = txtBxSource.Text;
+                    //if (!Directory.Exists(targetPath))
+                    //{
+                    //    Directory.CreateDirectory(targetPath);
+                    //}
+                    //foreach (var srcPath in Directory.GetFiles(sourcePath))
+                    //{
+                    //    File.Copy(srcPath, srcPath.Replace(sourcePath, targetPath), true);
+                    //}
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSourceBrow_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+                txtBxSource.Text = folderPath;
+            }
+        }
+
+        
+    }
+}
